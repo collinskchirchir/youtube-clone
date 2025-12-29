3148b64c513a31637c8f0be0a3f9430615aadb0f
Collins Chirchir <collinskchirchir@gmail.com>
2025-12-29 12:20:18 +0300

    home sidebar and layout

---
+++ src/modules/home/ui/components/home-sidebar/index.tsx
@@ -0,0 +1,27 @@
+// import { SignedIn } from '@clerk/nextjs';
+
+import { Separator } from '@/components/ui/separator';
+import { Sidebar, SidebarContent } from '@/components/ui/sidebar';
+
+import { MainSection } from './main-section';
+import { PersonalSection } from './personal-section';
+
+// import { SubscriptionsSection } from "./subscriptions-section"
+
+export const HomeSidebar = () => {
+  return (
+    <Sidebar className='z-40 border-none pt-16' collapsible='icon'>
+      <SidebarContent className='bg-background'>
+        <MainSection />
+        <Separator />
+        <PersonalSection />
+        {/*<SignedIn>*/}
+        <>
+          {/*<Separator />*/}
+          {/*<SubscriptionsSection />*/}
+        </>
+        {/*</SignedIn>*/}
+      </SidebarContent>
+    </Sidebar>
+  );
+};
--- src/modules/home/ui/layouts/home-layout.tsx
+++ src/modules/home/ui/layouts/home-layout.tsx
@@ -1,8 +1,7 @@
 import { SidebarProvider } from '@/components/ui/sidebar';
 
-import { HomeNavbar } from '../components/home-navbar'; // import { HomeSidebar } from "../components/home-sidebar";
-
-// import { HomeSidebar } from "../components/home-sidebar";
+import { HomeNavbar } from '../components/home-navbar';
+import { HomeSidebar } from '../components/home-sidebar';
 
 interface HomeLayoutProps {
   children: React.ReactNode;
@@ -14,7 +13,7 @@ export const HomeLayout = ({ children }: HomeLayoutProps) => {
       <div className='w-full'>
         <HomeNavbar />
         <div className='flex min-h-screen pt-[4rem]'>
-          {/*<HomeSidebar />*/}
+          <HomeSidebar />
           <main className='flex-1 overflow-y-auto'>{children}</main>
         </div>
       </div>
