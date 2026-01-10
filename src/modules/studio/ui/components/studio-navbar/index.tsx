eed736d066d96d78104f3254c41143ad107bc08e
Collins Chirchir <collinskchirchir@gmail.com>
2026-01-10 15:55:08 +0300

    studio layout

---
+++ src/modules/studio/ui/components/studio-navbar/index.tsx
@@ -0,0 +1,33 @@
+import { AuthButton } from '@/modules/auth/ui/components/auth-button';
+import { StudioUploadModal } from '@/modules/studio/ui/components/studio-upload-modal';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { SidebarTrigger } from '@/components/ui/sidebar';
+
+export const StudioNavbar = () => {
+  return (
+    <nav className='fixed left-0 right-0 top-0 z-50 flex h-16 items-center border-b bg-white px-2 pr-5 shadow-md'>
+      <div className='flex w-full items-center gap-4'>
+        {/* Menu and Logo */}
+        <div className='flex flex-shrink-0 items-center'>
+          <SidebarTrigger />
+          <Link prefetch href='/' className='hidden md:block'>
+            <div className='flex items-center gap-1 p-4'>
+              <Image src='/logo.svg' alt='Logo' width={32} height={32} />
+              <p className='text-xl font-semibold tracking-tight'>Studio</p>
+            </div>
+          </Link>
+        </div>
+
+        {/* Spacer */}
+        <div className='flex-1' />
+
+        <div className='flex flex-shrink-0 items-center gap-4'>
+          <StudioUploadModal />
+          <AuthButton />
+        </div>
+      </div>
+    </nav>
+  );
+};
